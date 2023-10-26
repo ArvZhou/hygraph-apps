dd2700576026bfb9727c30e08c1f65b75a8031d8
Arvin <ar.zhou@outlook.com>
2023-10-26 10:39:54 +0800

    feat: remove repo name

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
-const repo = 'hygraph-apps'
-const assetPrefix = `/${repo}/`
-const basePath = `/${repo}`
+// const repo = 'hygraph-apps'
+// const assetPrefix = `/${repo}/`
+// const basePath = `/${repo}`
 
 const nextConfig = {
     trailingSlash: true,
-    assetPrefix: assetPrefix,
-    basePath: basePath,
+    // assetPrefix: assetPrefix,
+    // basePath: basePath,
     output: 'export',
 }
 
